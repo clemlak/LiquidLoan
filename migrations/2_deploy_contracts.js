7be4ff1868e16f40011b88189055018acb749fd3
Clement Lakhal <clementlakhal@gmail.com>
2018-12-19 09:16:44 +0100

    Updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,10 @@
 /* global artifacts */
 
 const LiquidLoan = artifacts.require('./core/LiquidLoan');
+const snowflakeAddress = '0x7EdA95f86D49ac97D2142Cb3903915835160efEe';
 
 function deployContracts(deployer) {
-  deployer.deploy(LiquidLoan);
+  deployer.deploy(LiquidLoan, snowflakeAddress);
 }
 
 module.exports = deployContracts;
